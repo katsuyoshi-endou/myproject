1b82f5d129ef63c4c1a7fd5a8583f2ec957eb96b
endou katsuyoshi <endkatsu@vesta.ocn.ne.jp>
2019-06-30 20:14:38 +0900

    add SBAW

---
+++ v1/common_script.js
@@ -0,0 +1,116 @@
+﻿$(function(){
+	$document = $(document);
+	
+
+	/* Excelダウンロードボタン */
+	var xlBtnid = _data["sheet"].layoutCd;
+	if ( xlBtnid == "lay-cpln-v1" ){
+		$(".xlDownload").attr("id", "CPLN");
+	}
+  else if ( xlBtnid == "lay-jksk-v1" ){
+		$(".xlDownload").attr("id", "JKSK");
+	}
+	else {
+		$(".xlDownload").remove();
+	}
+  
+  
+	/* 編集ボタン */
+	$('button.cspe').each(function(){
+		var chkfilid = "Fill--" + $(this).attr("data-cspe") + "_edit"; 
+		if ( $.inArray( chkfilid, writeFillList ) == -1 ) {
+			$(this).remove();
+		}
+	})
+
+  // SB 用(編集画面タブ切換え)
+	var tabpages = $('.tabpage');
+  tabpages.hide();
+  var tabId = "tab01"; //編集画面ではtab01が初期値として設定とする。
+	$('#'+tabId).addClass("active");
+  $('.tabpage.'+tabId).addClass("active").show();
+  
+  // SB 用(編集画面タブ切換え)
+  $document.on("click", ".cstab ul li a",function(event) {
+    event.preventDefault();
+    tabpages.hide();
+    $(".cstab ul li").removeClass("active");
+    var nextCls = "." + $(this).parent().attr("id");// tab01
+    var nextId  = "#" + $(this).parent().attr("id");// tab01
+    $(nextCls).show();
+    $(nextId).addClass("active");
+  });
+  
+  // SB日付
+  // $(".datepicker").datepicker({
+	// 	//showButtonPanel: true,
+	// 	changeYear: true,
+	// 	changeMonth: true
+	// 	//minDate: "-1M",
+	// 	//maxDate: "+1M",
+	// 	//firstDay: 1
+	// });
+	// $('.datepicker').datepicker("option", "dateFormat", 'yy/mm/dd' );
+	
+
+});	
+
+// error message
+function errMsg(msgID) {
+	var labels = _data["label"];
+	var msgs = {
+	// "MkhyMokErrorMsg" : labels["MkhyMokErrorMsg"],
+	// "MkhyWeiErrorMsg" : labels["MkhyWeiErrorMsg"],
+	}
+	return msgs[msgID];
+}
+
+function customPartialEditCheck(cspe, cspearg) {
+	return true;
+}
+
+function customForwardCheck() {
+	try {
+		if ( _data["sheet"].layoutCd == 'lay-jksk-v1' ){
+			if (needsCheck("Fill--chk_necessary_mkhy_mok_hon")) {
+			}
+		}
+	} catch (e) {
+		alert(e);
+		return false;
+	}
+	return true;
+}
+
+function customForwardCheck(actioncd) {
+/* 申請時必須チェック */
+  var result = true;
+  if ( _data["sheet"].layoutCd == 'lay-jksk-v1') {
+    if(actioncd == "FORWARD"){
+      if (needsCheck("Fill--chk_necessary_jksk")) {
+      }
+    }
+  }
+  return result;
+}
+
+function customPartialEditCheck(cspe, cspearg) {
+/* 保存時必須チェック */
+  $('.sheet-section .chkNG').removeClass('chkNG');
+  var result = true;
+  if(cspe ===  "skill_gyomukbn"){
+    result = checkSkillGyomukbn();
+  }
+  if(cspe ===  "skill_zokusei"){
+    result = checkSkillZokusei();
+  }
+  if(cspe ===  "skill_check"){
+    result = checkSkillCheck();
+  }
+  if(cspe === "gymkn_gyomukbn") {
+    result = checkGyomuGyomuKbn();
+  }
+  return result;
+}
+
+
---
+++ v1/jksk_script.js
@@ -0,0 +1,79 @@
+﻿/* eslint-disable vars-on-top */
+$(function() {
+  var $document = $(document);
+
+  // SB 用(編集画面タブ切換え)
+  var tabpages = $(".tabpage");
+  tabpages.hide();
+  var tabId = "tab01";
+
+  // 編集画面ではtab01が初期値として設定とする。
+  $("#" + tabId).addClass("active");
+  $(".tabpage." + tabId)
+    .addClass("active")
+    .show();
+
+  // SB 用(編集画面タブ切換え)
+  $document.on("click", ".cstab ul li a", function(event) {
+    event.preventDefault();
+    tabpages.hide();
+    $(".cstab ul li").removeClass("active");
+    var nextCls =
+      "." +
+      $(this)
+        .parent()
+        .attr("id");
+    var nextId =
+      "#" +
+      $(this)
+        .parent()
+        .attr("id");
+    $(nextCls).show();
+    $(nextId).addClass("active");
+  });
+});
+
+/**
+ * 
+ */
+function checkJkskRequired() {
+  var bErr = false;
+
+  $( ".required" ).each( function( i, elem ) {
+    if ( elem.text() === "" ) {
+      bErr = true;
+    }
+  });
+
+  if ( bErr ) {
+    alert( "必須項目が未入力です。" );
+  }
+
+  return !bErr;
+}
+
+function checkJkskSkill() {
+  var elem1 = $( "input[name='jksk_skill_lang_02_nm']" );
+  var elem2 = $( "input[name='jksk_skill_lang_02_nm_other']" );
+
+  if ( !checkRelatedOthersRequire( elem1, elem2, "その他" )) {
+    alert( "「その他」を選択した場合、該当設問に回答してください。" );
+    return false;
+  }
+  return true;
+}
+
+/**
+ * elem1の要素の値が、valueであるとき、elem2の要素の値が未入力であるかをチェックする
+ */
+function checkRelatedOthersRequire( elem1, elem2, value ) {
+  if ( elem1 === undefined || elem2 === undefined ) {
+    return true;
+  }
+
+  if ( elem1.val() === value && elem2.val() === "" ) {
+    return false;
+  }
+  return true;
+}
+
