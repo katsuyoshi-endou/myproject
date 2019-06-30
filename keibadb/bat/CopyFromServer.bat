6410213184846872674fd437d68fd05d56c2c794
endou katsuyoshi <endkatsu@vesta.ocn.ne.jp>
2019-06-30 20:13:02 +0900

    clean

--- keibadb/bat/CopyFromServer.bat
+++
@@ -1,5 +0,0 @@
-copy /Y S:\競馬予想\データベース\*.erm C:\pleiades\workspace\keibadb
-copy /Y S:\競馬予想\データベース\keibadb.sql C:\pleiades\workspace\keibadb
-copy /Y S:\競馬予想\データベース\keibadb_v2.sql C:\pleiades\workspace\keibadb
-copy /Y S:\競馬予想\データベース\master_data.sql C:\pleiades\workspace\keibadb
-pause
